b3c2a2325ddfd21286160f35fa6cdc17ef5051b0
Akshay Kumar <email.akshay98@gmail.com>
2019-10-20 15:18:17 +0530

    port fix

--- index.js
+++ index.js
@@ -75,4 +75,4 @@ app.get('getBalancePerZone', function(req, res) {
     }); 
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000 || process.env.PORT, () => console.log('Example app listening on port 3000!'))
